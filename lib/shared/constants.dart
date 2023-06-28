783df59965939f86b74cf71e64a45aac4653c0b6
thomson <thomsonstanesdelta@gmail.com>
2023-06-28 15:19:17 +0530

    initial commit

---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:906281483137:ios:0638f849060e1f7e620680";
+  static String apiKey = "AIzaSyAXG1e9FAo1YwfR-zUZVchTDOu61mKjAHc";
+  static String messagingSenderId = "906281483137";
+  static String projectId = "notificationtest-d40c3";
+  final primaryColor = const Color(0xff005613);
+}
